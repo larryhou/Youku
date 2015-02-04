cbb7802183dffb12483b8c307653a5ceed663a02
larryhou <larryhou@foxmail.com>
2015-02-04 23:41:27 +0800

    optimize

--- arrange.sh
+++ arrange.sh
@@ -3,17 +3,17 @@ find . -depth 2 -iname '*.flv' | grep ' [0-9]\{1,\}\.flv$' | while read line
 do
 	epi=$(echo ${line} | awk -F/ '{print $2}')
 	
-	dir=$(echo ${epi} | awk '{print $1}')
+	dir=$(echo ${epi} | sed 's/ [0-9]\{2,\}$//')
 	echo ${dir}
 	
 	if [ ! -d "${dir}" ]
 	then
-		mkdir ${dir}
+		mkdir "${dir}"
 	fi
 	
 	if [ ! -f "${dir}/${epi}" ]
 	then
-		mv -fv "${epi}" ${dir}/
+		mv -fv "${epi}" "${dir}/"
 		mv -fv "${dir}/${epi}/${epi}.flv" "${dir}/${epi}.flv"
 	fi
 done
